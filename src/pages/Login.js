bad976c5b56d52b08aad903e72b8d3c48e3b918c
karan chaudhary <karanchy95@gmail.com>
2024-05-29 22:00:41 +0545

    form markup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,13 +2,27 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <div>
+    <div className="flex">
+      <div className="basis-[30%]">
+        <div>
+          <h2>
+            blofgj
+          </h2>
+          <div><p>Welcome to our App</p> <a>Register</a></div>
+        </div>
+
         <form>
-          testt
+          <label>Email</label>
+          <input type="text"></input>
+          <label>Password</label>
+          <input type="password"></input>
+          <input type="checkbox"></input>
+          <button type="submit">Login</button>
+          <button type="submit">Login With Brocess</button>
+
         </form>
       </div>
-      <div>
+      <div className="flex-1">
         <img></img>
       </div>
     </div>
